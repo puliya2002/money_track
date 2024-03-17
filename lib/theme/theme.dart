b6387f7abd61b0e63d572bee29c718411a2b6483
puliya2002 <141041873+puliya2002@users.noreply.github.com>
2024-03-17 12:46:02 +0530

    theme data

---
+++ lib/theme/theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme.light(
+    background: Color(0xFFf3f6f9),
+
+    onBackground: Color(0xFF363636),
+
+
+    primary: Color(0xFF2291fd),
+    secondary: Color(0xFF6dd2ff),
+    primaryContainer: Colors.white,
+    secondaryContainer: Color(0xFF363636),
+    shadow: Color(0xFF2291fd),
+
+
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+      background: Colors.grey.shade900,
+      onBackground: Color(0xFFdedede),
+
+      primary: Color(0xFF015eea),
+      secondary: Color(0xFF009ffa),
+      primaryContainer: Color(0xFF383838),
+      secondaryContainer: Colors.black,
+      shadow: Color(0xFF000046).withOpacity(0.5),
+    )
+
+);
